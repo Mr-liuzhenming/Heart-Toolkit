0ae13007ea80ea8ae8908cdae0f457b449d2e981
Liu from the production team <2124619132@qq.com>
2022-12-27 22:09:40 +0800

    初始化模块
    __init__.py 文件的作用是将所有模块中的函数组合起来，提供一个统一的接口供用户使用。例如，你可以通过调用 summarize 函数来生成文本摘要，而不用关心摘要生成过程中使用了哪些模块中的函数。

---
+++ _init_.py
@@ -0,0 +1,14 @@
+from .text_reader import read_text
+from .keyword_extractor import extract_keywords
+from .pattern_builder import build_patterns
+from .pattern_matcher import match_patterns
+from .summary_generator import generate_summary
+
+def summarize(text):
+    """生成文本摘要"""
+    tokens = read_text(text)
+    keywords = extract_keywords(tokens)
+    patterns = build_patterns(keywords)
+    matched_patterns = match_patterns(tokens, patterns)
+    summary = generate_summary(tokens, matched_patterns)
+    return summary
